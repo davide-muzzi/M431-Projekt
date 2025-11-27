b0988b32222a6417761a23c5ff2c3a1d1d3c6ee5
Ghostylace <fescherb@outlook.com>
2025-11-27 10:35:27 +0100

    Added Services and stuff for Authentication

---
+++ Shared/DTOs/Auth/RegisterRequestDTO.cs
@@ -0,0 +1,8 @@
+﻿namespace Shared.DTOs.Auth;
+
+public class RegisterRequestDTO
+{
+    public string UserName { get; set; } = string.Empty;
+    public string Email { get; set; } = string.Empty;
+    public string Password { get; set; } = string.Empty;
+}
